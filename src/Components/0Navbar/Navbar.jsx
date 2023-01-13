8eb2623e4b0b2ef15dbf60aa4956018e44d84543
thearnabsaha <thearnabsaha201@gmail.com>
2023-01-14 02:22:36 +0530

    new Navbar added

--- src/Components/0Navbar/Navbar.jsx
+++ src/Components/0Navbar/Navbar.jsx
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom'
 import logo from '../../Assets/logo.webp'
 import "./Navbar.scss"
 import { GoThreeBars } from "react-icons/go";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+
 const Navbar = () => {
   const [sticky, setSticky] = useState(false)
   const [open, setOpen] = useState(false)
@@ -17,6 +20,45 @@ const Navbar = () => {
   const addClass=()=>{
     setOpen(!open)
   }
+
+  const [state, setState] = React.useState({
+    left: false
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <div className={true?"close mNav":"open mNav"}>
+          <div className="logo">
+              <img src={logo} alt="" />
+              <h1>Grevelops</h1>
+          </div>
+          <hr />
+          <div className="linkas">
+            <Link to="/" className='link'>Home</Link>
+            <Link to="/portfolio/all" className='link'>Portfolio</Link>
+            <Link to="/about" className='link'>About</Link>
+            <Link to="/contact" className='link'>Contact</Link>
+          </div>
+        </div>
+    </Box>
+  );
+
   return (
     <div className='a'>
         <div className={open?"close mNav":"open mNav"}>
@@ -30,7 +72,6 @@ const Navbar = () => {
             <Link to="/portfolio" className='link'>Portfolio</Link>
             <Link to="/about" className='link'>About</Link>
             <Link to="/contact" className='link'>Contact</Link>
-
           </div>
         </div>
         <div id="Navbar" className={sticky?"sticky":"fixed"}>
@@ -44,8 +85,20 @@ const Navbar = () => {
                 <Link to="/about" className='link'>About Us</Link>
                 <Link to="/contact" className='link contact'>Contact Us</Link>
                 <div className="iconBox" onClick={addClass} >
-                  <GoThreeBars className='icon'/>
+                  {/* <GoThreeBars className='icon'/> */}
                 </div>
+                {["left"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <GoThreeBars className='iconf' onClick={toggleDrawer(anchor, true)}>{anchor}</GoThreeBars>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
             </div>
         </div>
     </div>
@@ -53,3 +106,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
